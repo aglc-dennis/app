c4278a00d0c3161cfe858d062fd283b288f454c6
aglc-dennis <Dennis.Mah@aglc.ca>
2014-03-26 12:38:20 -0600

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -1,10 +1,29 @@
-﻿namespace app.web.core
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace app.web.core
 {
   public class CommandRegistry : IFindCommandsToProcessRequests
   {
-    public IProcessOneRequest get_command_that_can_process(IProvideDetailsAboutARequest request)
+      private IEnumerable<IProcessOneRequest> requests;
+
+      public CommandRegistry(IEnumerable<IProcessOneRequest> requests)
+      {
+          this.requests = requests;
+      }
+
+      public IProcessOneRequest get_command_that_can_process(IProvideDetailsAboutARequest request)
     {
-      throw new System.NotImplementedException();
+          foreach (var oneRequest in requests)
+          {
+              if (oneRequest.can_process(request))
+              {
+                  return oneRequest;
+              }
+          }
+
+          throw new Exception("WHAAAAT I COULDN'T FIND ANYTHING SO I SHOULD THROW AN EXCEPTION DUE TO OUR ASSUMPTIONS");
     }
   }
 }
