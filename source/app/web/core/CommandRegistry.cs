c4a4104ddd621ab725b7983899eb2abddcc5ce05
aglc-dennis <Dennis.Mah@aglc.ca>
2014-03-26 12:50:19 -0600

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -6,15 +6,25 @@ namespace app.web.core
   public class CommandRegistry : IFindCommandsToProcessRequests
   {
       private readonly IEnumerable<IProcessOneRequest> all_commands;
+      private readonly ICreateASpecialCaseWhenACommandIsNotFound special_case;
 
-      public CommandRegistry(IEnumerable<IProcessOneRequest> all_commands)
+
+      public CommandRegistry(IEnumerable<IProcessOneRequest> all_commands, ICreateASpecialCaseWhenACommandIsNotFound specialCase)
       {
           this.all_commands = all_commands;
+          special_case = specialCase;
       }
 
       public IProcessOneRequest get_command_that_can_process(IProvideDetailsAboutARequest request)
       {
-          return all_commands.First(x => x.can_process(request));
+          var result = all_commands.FirstOrDefault(x => x.can_process(request));
+
+          if (result == null)
+          {
+              return special_case.Invoke(request);
+          }
+
+          return result;
       }
   }
 }
